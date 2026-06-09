4d8251b480ae380608cb90ec0f31ce71cec929e4
lakshmipriyakg <lakshmipriyakg2021@gmail.com>
2026-06-09 22:49:37 +0530

    Update top.v

--- day2/encoder/top.v
+++ day2/encoder/top.v
@@ -1,1 +1,14 @@
+`timescale 1ns / 1ps
+module encoder4_2(input[3:0]i,output reg [1:0]y);
+always@(*)
+begin
+case(i)
+4'b0001:y=2'b00;
+4'b0010:y=2'b01;
+4'b0100:y=2'b10;
+4'b1000:y=2'b11;
+default:y=2'b00;
+endcase
+end
+endmodule
 
