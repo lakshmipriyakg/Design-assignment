9ddb89797ead7bcfdb31bc12035843161859336c
lakshmipriyakg <lakshmipriyakg2021@gmail.com>
2026-06-09 22:54:48 +0530

    Update top.v

--- day2/Dflipflop/top.v
+++ day2/Dflipflop/top.v
@@ -1,1 +1,18 @@
-
+`timescale 1ns / 1ps
+module dff(
+input d,rst,clk,output reg q,qbar);
+always@(posedge clk)begin
+if(rst) begin
+q<=1'b0;
+qbar<=1'b1;
+end
+else if(d==0) begin
+q<=1'b0;
+qbar<=1'b1;
+end
+else if(d==1) begin
+q<=1'b1;
+qbar<=1'b0;
+end   
+end 
+endmodule
