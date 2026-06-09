0ac58f20bb24440c0675b8f5620737fac5cef23f
lakshmipriyakg <lakshmipriyakg2021@gmail.com>
2026-06-10 00:15:01 +0530

    Update SRtb_top.v

--- day2/SRflipflop/SRtb_top.v
+++ day2/SRflipflop/SRtb_top.v
@@ -1,28 +1,39 @@
 `timescale 1ns / 1ps
-module srff_tb();
-    reg s_tb,r_tb,rst_tb,clk_tb;
-    wire q_tb,qbar_tb;
-    srff dut(s_tb,r_tb,rest_tb,clk_tb,q_tb,qbar_tb);
-    initial
-    begin
-    {s_tb,r_tb,rst_tb,clk_tb}=0;
-    end
-    always #5 clk_tb= ~ clk_tb;
-    initial
-    begin
-    rst_tb=1;
-    #10;
-    rst_tb=0;
-    s_tb=0;
-    r_tb=0;
-    #10;
-    s_tb=0;
-    r_tb=1;
-    #10;
-    s_tb=1;
-    r_tb=1;
-    #10;
-    s_tb=1;
-    r_tb=1;
-    end
+module sr_ff_tb();
+reg s_tb, r_tb, rst_tb, en_tb, clk_tb;
+wire q_tb, qbar_tb;
+
+sr_ff dut(.s(s_tb),.r(r_tb),.rst(rst_tb),.en(en_tb),.clk(clk_tb),.q(q_tb),.qbar(qbar_tb));
+
+always #5 clk_tb = ~clk_tb;
+initial 
+begin
+    clk_tb = 0;
+    s_tb = 0; r_tb = 0; rst_tb = 0; en_tb = 0;
+    
+    #10 en_tb = 1; rst_tb = 1;
+    #10 rst_tb = 0;
+    
+    #10 s_tb = 0; r_tb = 0;
+    #10 s_tb = 0; r_tb = 1;
+    #10 s_tb = 1; r_tb = 0;
+    #10 s_tb = 1; r_tb = 1;
+    
+    #10 en_tb = 0;
+    #10 s_tb = 1; r_tb = 0;
+    #10 s_tb = 0; r_tb = 1;
+    
+    #10 en_tb = 1; rst_tb = 1;
+    #10 rst_tb = 0;
+    #10 s_tb = 1; r_tb = 0;
+    #10 s_tb = 0; r_tb = 1;
+    #10 s_tb = 0; r_tb = 0;
+    #10 s_tb = 1; r_tb = 1;
+
+end
+
+initial 
+begin
+$monitor($time," clk=%b en=%b rst=%b s=%b r=%b | q=%b qbar=%b",clk_tb, en_tb, rst_tb, s_tb, r_tb, q_tb, qbar_tb);
+end
 endmodule
