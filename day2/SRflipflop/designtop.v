5cbd08e5c64787e5d56b808f445bd31d1d8243ca
lakshmipriyakg <lakshmipriyakg2021@gmail.com>
2026-06-10 00:16:02 +0530

    Update designtop.v

--- day2/SRflipflop/designtop.v
+++ day2/SRflipflop/designtop.v
@@ -1,26 +1,28 @@
 `timescale 1ns / 1ps
-module srff(input s,r,rst,clk,output reg q,qbar);
-always@(posedge clk) begin
-if (rst) begin
-q<=1'b0;
-qbar<=1'b1;
+module sr_ff(input s,r,rst,en,clk,output reg q,qbar);
+always@(posedge clk)
+begin
+if(en)begin
+  if(rst)begin
+  q<=1'b0;
+  qbar<=1'b1;
+  end
+  else if(s==0&&r==0)begin
+  q<=q;
+  qbar<=qbar;
+  end
+  else if(s==0&&r==1)begin
+  q<=1'b0;
+  qbar<=1'b1;
+  end
+  else if(s==1&&r==0)begin
+  q<=1'b1;
+  qbar<=1'b0;
+  end
+  else if(s==1&&r==1) begin
+  q<=1'bx;
+  qbar<=1'bx;
+  end
 end
-else if(s==0 &&r==0) begin
-q<=q;
-qbar<=qbar;
 end
-else if(s==0 && r==1)begin
-q<=1'b0;
-qbar<=1'b1;
- end
-else if(s==1 && r==0)begin
- q<=1'b1;
- qbar<=1'b0;
- end
- else if(s==1 && r==1)begin
-q<=1'bx;
-qbar<=1'bx;
- end
- end
-
 endmodule
