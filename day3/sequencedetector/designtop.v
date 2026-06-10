43ecbba2ad800da0ea69181786cb2d89d39eedf9
lakshmipriyakg <lakshmipriyakg2021@gmail.com>
2026-06-10 14:53:25 +0530

    Update designtop.v

--- day3/sequencedetector/designtop.v
+++ day3/sequencedetector/designtop.v
@@ -1,1 +1,58 @@
+`timescale 1ns / 1ps
+module sequence_detector(input clk,rst,din,output reg det);
+parameter idle=2'b00;
+parameter s1=2'b01;
+parameter s2=2'b10;
+parameter s3=2'b11;
+reg [1:0] ps,ns;
+//present state logic behaviour level
+always@(posedge clk)
+begin
+if(rst) begin
+ps<=idle;
+end
+else
+ps<=ns;
+end
 
+//next state logic
+ always@(*)
+    begin
+    case(ps)
+    idle: begin
+          det=0;
+          if(din==0)
+            ns=idle;
+          else 
+            ns=s1;
+      end
+      s1: begin
+          det=0;
+          if(din==0)
+           ns=idle;
+          else
+           ns=s2;
+          end
+       s2: begin
+           det=0;
+           if(din==0)
+             ns=idle;
+           else
+             ns=s3;
+           end
+       s3: begin
+           if(din==0)begin
+             det=1;
+             ns=idle;
+             end
+           else
+             ns=s3;
+           end
+      default: begin
+            det=0;
+            ns=idle;
+            end
+      endcase       
+    end
+    
+endmodule
