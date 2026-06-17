fd51887bdb7d2cd70aa42e024de931a5b0db03b7
lakshmipriyakg <lakshmipriyakg2021@gmail.com>
2026-06-17 23:00:21 +0530

    Update transaction.sv

--- day7/APB/transaction.sv
+++ day7/APB/transaction.sv
@@ -1,1 +1,14 @@
 
+class transaction;
+    rand bit [31:0] paddr;
+    rand bit [31:0] pwdata;
+    rand bit        pwrite;
+         bit [31:0] prdata;
+
+    // Constrain addresses to our memory size
+    constraint addr_c { paddr < 256; }
+    
+    function void display(string name);
+        $display("[%s] Addr: %0h, Data: %0h, Write: %0b", name, paddr, (pwrite ? pwdata : prdata), pwrite);
+    endfunction
+endclass
