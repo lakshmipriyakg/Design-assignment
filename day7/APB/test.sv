6263c2ce5b5406fa5cadf1fa007b0f4c5830aab3
lakshmipriyakg <lakshmipriyakg2021@gmail.com>
2026-06-17 22:59:53 +0530

    Update test.sv

--- day7/APB/test.sv
+++ day7/APB/test.sv
@@ -1,53 +1,13 @@
 
-`include "interface.sv"
-`include "transaction.sv"
-`include "generator.sv"
-`include "driver.sv"
-`include "monitor.sv"
-`include "scoreboard.sv"
-`include "environment.sv"
-`include "test.sv"
+class test;
+    environment env;
 
-module testbench;
-    logic pclk;
-    logic presetn;
+    function new(virtual apb_if vif);
+        env = new(vif);
+    endfunction
 
-    // Clock generation
-    always #5 pclk = ~pclk;
-
-    // Interface instantiation
-    apb_if intf(pclk, presetn);
-
-    // DUT instantiation
-    apb_slave dut (
-        .pclk(intf.pclk),
-        .presetn(intf.presetn),
-        .paddr(intf.paddr),
-        .pwrite(intf.pwrite),
-        .psel(intf.psel),
-        .penable(intf.penable),
-        .pwdata(intf.pwdata),
-        .prdata(intf.prdata),
-        .pready(intf.pready),
-        .pslverr(intf.pslverr)
-    );
-
-    test t1;
-
-    initial begin
-        pclk = 0;
-        presetn = 0;
-        
-        // Reset sequence
-        #15 presetn = 1;
-        
-        t1 = new(intf);
-        t1.run();
-    end
-
-    // Enable wave dumps for EDA Playground
-    initial begin
-        $dumpfile("dump.vcd");
-        $dumpvars;
-    end
-endmodule
+    task run();
+        env.gen.repeat_count = 10; // Generate 10 transactions
+        env.run();
+    endtask
+endclass
